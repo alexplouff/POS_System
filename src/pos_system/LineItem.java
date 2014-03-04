1fe85d5ac549c0771293eba02822da78372586d9
Alex <Alex@10.30.17.60>
2014-03-04 13:40:58 -0800

    Trouble running

--- src/pos_system/LineItem.java
+++ src/pos_system/LineItem.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 package pos_system;
 
@@ -13,53 +9,37 @@ package pos_system;
 public class LineItem {
     
     private Product product;
+    DiscountStrategy ds;
     
     public LineItem(Product product){
         setProduct(product);
     }
 
-    public String getProductDescription() {
-        return product.getProductDescription();
-    }
-
     public void setProduct(Product product) {
         this.product = product;
     }
-
-    public String getProductID() {
-        return productID;
-    }
-
-    public void setProductID(String productID) {
-        this.productID = productID;
-    }
-
-    public double getProductPrice() {
-        return productPrice;
-    }
-
-    public void setProductPrice(double productPrice) {
-        this.productPrice = productPrice;
-    }
-
-    public DiscountStrategy getDiscountStrategy() {
-        return discountStrategy;
-    }
-
-    public void setDiscountStrategy(DiscountStrategy discountStrategy) {
-        this.discountStrategy = discountStrategy;
+    
+    public String getProductDescription() {
+        return product.getProductDescription();
     }
     
+    public String getProductID(){
+        return product.getProductID();
+    }
     
+    public double getProductPrice(){
+        return product.getProductPrice();
+    }
+    
+    public double getAmountSaved(){
+        return ds.getSavedAmount(product.getProductPrice(), 5);
+    }
+    
+    public double getTotalAfterDiscount(){
+        return ds.getTotalAfterDiscountedRate(product.getProductPrice(), 5);
+    }
     
     public static void main(String[] args) {
-        
+        LineItem l = new LineItem(  );
     }
-    
-    
-    
-    
-                  
-    
-    
 }
